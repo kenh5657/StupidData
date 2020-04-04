bd891924bc8aba03e114f6b8ebc16bbaf6e87b86
kth62 <kth62@cornell.edu>
2020-04-03 22:13:51 -0400

    some fckin changes

---
+++ ramp_gen.py
@@ -0,0 +1,31 @@
+import random as rand
+import numpy as np 
+
+filename = 'ubmark-K-ramp-4.dat'
+size = 800;
+src = []
+ref = []
+
+for x in range(0,size):
+    src.append(rand.randint(0, 10000))
+
+ref = sorted(src)
+
+form_src = 'int src[] = { \n'
+for elem in src:
+	form_src = form_src + str(elem) +',\n'
+form_src = form_src + '};\n\n'
+
+form_ref = 'int ref[] = { \n'
+for elem in ref:
+	form_ref = form_ref + str(elem) +',\n'
+form_ref = form_ref + '};'
+
+open(filename, 'w').close()
+file = open(filename,"w")
+file.write('// Data set for 800 randomly sorted items.\n \n')
+file.write('int size = ' + str(size) + ';\n \n')
+file.write(form_src)
+file.write(form_ref)
+
+file.close()
